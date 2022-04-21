a23f6b5e15bf415b4139b52e379811246e96ab54
tdindorf <t_dindorf@hotmail.com>
2022-04-21 14:48:27 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+import {sizeMySvg} from 'sizeMe.js';
+
+function myFun2(me) {
+	var svg = SVG(me);
+	var box = svg.group();
+	var defs = svg.defs();
+
+	box.circle(4).center(0, 0).fill("red");
+	
+	sizeMySvg(box, svg);
+}
+
+/*
+//********** RESIZE ************
+function sizeMySvg(box, svg, sH, sW) {
+	var bBox = box.bbox();
+	var vBox = bBox.x + " " + bBox.y + " " + bBox.width + " " + bBox.height;
+	svg.viewbox(vBox); // svg.js: viewbox; d3: viewBox
+	if (sH != 0) svg.height(sH);
+}
+*/
